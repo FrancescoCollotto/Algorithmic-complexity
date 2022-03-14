17c42fa9b3ce1553ba99fdfbb4b9eecf605279f1
Francesco <francescocollotto@gmail.com>
2022-03-14 11:26:39 +0000

    add createTestData function to create an array of arrays of numbers

--- timeFramework.js
+++ timeFramework.js
@@ -8,8 +8,17 @@ const timeFramework = (testFunc) => {
   return result;
 }
 
-const numbers = Array.from(Array(100000000).keys())
+// create 2d array of numbers increasing in dimension by step
+const createTestData = (step, limit) => {
+  const listOfArrays = [];
+  for(let i = step; i <= limit; i += step) {
+    const arrayOfNumbers = Array.from(Array(i).keys());
+    listOfArrays.push(arrayOfNumbers);
+  }
+  return listOfArrays;
+}
 
-const timing = timeFramework(() => numbers.reverse());
-console.log(timing)
+// const timing = timeFramework(() => numbers.reverse());
+// console.log(timing)
+console.log(createTestData(1, 10))
 
