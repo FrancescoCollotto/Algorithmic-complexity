58083eb52c12ef6d5db55557676c8ac8755f7a5f
Francesco <francescocollotto@gmail.com>
2022-03-14 11:02:59 +0000

    add timeFramework function

--- timeFramework.js
+++ timeFramework.js
@@ -1,5 +1,15 @@
-const start = Date.now();
-// function to run
-const end = Date.now();
-const result = start - end;
-console.log(result)
+
+
+const timeFramework = (testFunc) => {
+  const start = Date.now();
+  testFunc();
+  const end = Date.now();
+  const result = end - start;
+  return result;
+}
+
+const numbers = Array.from(Array(100000000).keys())
+
+const timing = timeFramework(() => numbers.reverse());
+console.log(timing)
+
