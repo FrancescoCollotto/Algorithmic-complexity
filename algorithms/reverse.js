88532b3ac04221f49e596af5e59e4ad4f1ea0569
Francesco <francescocollotto@gmail.com>
2022-03-16 12:09:49 +0000

    refactor reverse function

--- algorithms/reverse.js
+++ algorithms/reverse.js
@@ -2,21 +2,10 @@
 
 const reverse = (arr) => {
   const reversedArray = [];
-  if (arr.length <= 1) {
-    return arr;
-  } else {
-    for(let i = arr.length -1; i >= 0; i--) {
-      reversedArray.push(arr[i]);
-    }
-    return reversedArray;
+  for(let i = arr.length -1; i >= 0; i--) {
+    reversedArray.push(arr[i]);
   }
+  return reversedArray;
 }
 
 module.exports = reverse;
-
-/* INPUT         |        OUTPUT
-  []             |  []
-  [1]            |  [1]
-  [1,2,3,4]      | [4,3,2,1]
-*/
-
