76c796066012786c26684c7ee8c0cc3de9cae6ad
Francesco <francescocollotto@gmail.com>
2022-03-16 12:05:19 +0000

    implement basic logic to reverse

--- algorithms/reverse.js
+++ algorithms/reverse.js
@@ -1,10 +1,14 @@
 // Reverse an array without using the built-in function
 
 const reverse = (arr) => {
-  if (arr.length === 0) {
-    return [];
+  const reversedArray = [];
+  if (arr.length <= 1) {
+    return arr;
   } else {
-    return [1];
+    for(let i = arr.length -1; i >= 0; i--) {
+      reversedArray.push(arr[i]);
+    }
+    return reversedArray;
   }
 }
 
@@ -15,3 +19,4 @@ module.exports = reverse;
   [1]            |  [1]
   [1,2,3,4]      | [4,3,2,1]
 */
+
--- algorithms/tests/reverse.test.js
+++ algorithms/tests/reverse.test.js
@@ -8,4 +8,8 @@ describe('reverse', () => {
   it('return original array if length equal 1', () => {
     expect(reverse([1])).toEqual([1]);
   })
+
+  it('reverse the array elements position', () => {
+    expect(reverse([1,2,3,4])).toEqual([4,3,2,1]);
+  })
 })
